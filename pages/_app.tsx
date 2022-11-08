cff4d52a20b7315646c7b88df5418d5146a14f71
Ricardo Menotti <86802680+menottiRicardo@users.noreply.github.com>
2022-11-08 14:34:50 -0500

    dashboard added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,21 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { AnimatePresence } from "framer-motion";
+import { NextPage } from "next";
+import { ReactElement, ReactNode } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
   return (
     <AnimatePresence mode="wait">
-      <Component {...pageProps} />
+      {getLayout(<Component {...pageProps} />)}
     </AnimatePresence>
   );
 }
