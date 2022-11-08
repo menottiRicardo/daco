ccde3cecce98301a840debc0ab815bbed9d1ec85
Ricardo Menotti <86802680+menottiRicardo@users.noreply.github.com>
2022-11-08 14:44:02 -0500

    missing icons

--- components/Cards/CardStats.tsx
+++ components/Cards/CardStats.tsx
@@ -1,6 +1,5 @@
 // @ts-nocheck
 import React from "react";
-import PropTypes from "prop-types";
 
 export default function CardStats({
   statSubtitle,
@@ -32,7 +31,7 @@ export default function CardStats({
                   statIconColor
                 }
               >
-                <i className={statIconName}></i>
+                <i className={`${statIconName}`}></i>
               </div>
             </div>
           </div>
@@ -56,29 +55,3 @@ export default function CardStats({
     </>
   );
 }
-
-CardStats.defaultProps = {
-  statSubtitle: "Traffic",
-  statTitle: "350,897",
-  statArrow: "up",
-  statPercent: "3.48",
-  statPercentColor: "text-emerald-500",
-  statDescripiron: "Since last month",
-  statIconName: "far fa-chart-bar",
-  statIconColor: "bg-red-500",
-};
-
-CardStats.propTypes = {
-  statSubtitle: PropTypes.string,
-  statTitle: PropTypes.string,
-  statArrow: PropTypes.oneOf(["up", "down"]),
-  statPercent: PropTypes.string,
-  // can be any of the text color utilities
-  // from tailwindcss
-  statPercentColor: PropTypes.string,
-  statDescripiron: PropTypes.string,
-  statIconName: PropTypes.string,
-  // can be any of the background color utilities
-  // from tailwindcss
-  statIconColor: PropTypes.string,
-};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
+const colors = require("tailwindcss/colors");
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      ...colors,
+    },
     extend: {
       fontFamily: {
         latoBold: "LatoBold",
@@ -11,6 +15,20 @@ module.exports = {
       animation: {
         "spin-slow": "spin 4s linear infinite",
       },
+      variants: [
+        "responsive",
+        "group-hover",
+        "focus-within",
+        "first",
+        "last",
+        "odd",
+        "even",
+        "hover",
+        "focus",
+        "active",
+        "visited",
+        "disabled",
+      ],
     },
   },
   plugins: [],
